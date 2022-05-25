a2ec70053935918858dfd5bf569cf085856fcba1
Rohann18 <rohann.bella@hotmail.fr>
2022-05-25 16:43:10 +0200

    Finalisation de la classe SalarieRepositoryTests

--- src/test/java/com/inti/SpringBootTD2Tests/repository/SalarieRepositoryTests.java
+++ src/test/java/com/inti/SpringBootTD2Tests/repository/SalarieRepositoryTests.java
@@ -2,6 +2,10 @@ package com.inti.SpringBootTD2Tests.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import com.inti.SpringBootTD2Tests.model.Salarie;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -38,5 +42,54 @@ public class SalarieRepositoryTests {
 		//Then
 		assertThat(savedSalarie).isNotNull();
 		assertThat(savedSalarie.getId()).isGreaterThan(1);
+	}
+	
+	@Test
+	public void testReadSalarie() {
+		//Given
+
+		//When
+		Salarie readSalarie = salarieRepository.getReferenceById(4);
+		//Then
+		assertThat(readSalarie).isNotNull();
+		assertThat(readSalarie.getId()).isEqualTo(4);
+		assertThat(readSalarie.getNom()).isEqualTo("Haire");
+		assertThat(readSalarie.getPrenom()).isEqualTo("Axel");
+		assertThat(readSalarie.getEmail()).isEqualTo("axel.haire@test.fr");
+	}
+	
+	@Test
+	public void testAllReadSalarie() {
+		//Given
+		List<Salarie> listSalarie = new ArrayList<Salarie>();
+		//When
+		listSalarie = salarieRepository.findAll();
+		//Then
+		assertThat(listSalarie).isNotNull();
+		assertThat(listSalarie).hasSize((int) salarieRepository.count());
+		assertThat(listSalarie.get(0).getClass()).hasSameClassAs(Salarie.class);
+	}
+	
+	@Test
+	public void testUpdateSalarie() {
+		//Given
+		Salarie s1 = salarieRepository.getReferenceById(5);
+		//When
+		s1.setNom("Jument");
+		Salarie s = salarieRepository.save(s1);
+		//Then
+		assertThat(s).isNotNull();
+		assertThat(s.getNom()).isEqualTo("Jument");
+		
+	}
+	
+	@Test
+	public void testDeleteSalarie() {
+		//Given
+		int id = 4;
+		//When
+		salarieRepository.deleteById(id);
+		//Then
+		assertThat(salarieRepository.existsById(id)).isFalse();
 	}
 }
